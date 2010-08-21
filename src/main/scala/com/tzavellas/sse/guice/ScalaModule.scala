efb1ce40495b4a45b5758e5e5b775a010719f44d
Spiros Tzavellas <spiros@tzavellas.com>
2010-08-22 00:51:59 +0300

    Added license headers.

--- src/main/scala/com/tzavellas/sse/guice/ScalaModule.scala
+++ src/main/scala/com/tzavellas/sse/guice/ScalaModule.scala
@@ -1,3 +1,7 @@
+/* ------------------ sse-guice ------------------ *\
+ * Licensed under the Apache License, Version 2.0. *
+ * Author: Spiros Tzavellas                        *
+\* ----------------------------------------------- */
 package com.tzavellas.sse.guice
 
 import com.google.inject._
